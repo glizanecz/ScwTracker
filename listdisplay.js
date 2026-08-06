8431741ef8682c7bff7e48ec5c9aee9d568f85fa
glizanecz <glizanecz@gmail.com>
2026-08-06 03:07:56 -0400

    maybe that was a problem? weirdly enough it did seem to sort but idk.

--- listdisplay.js
+++ listdisplay.js
@@ -3,7 +3,7 @@ function sortAutomatically(projects) {
         return findDateDistance(a.projDueTime, a.projDueDate) -
         findDateDistance(b.projDueTime, b.projDueDate)
     })
-    return tempArray
+    return projects
 }
 
 function findDateDistance(dueTime, dueDate) {
