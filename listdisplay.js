f2f11bc01b1efc4d31871bc8dc75b9380e8eac23
Greyson Lizanecz <glizanecz@gmail.com>
2026-08-06 22:22:11 -0400

    submit that change

--- listdisplay.js
+++ listdisplay.js
@@ -45,6 +45,9 @@ function displayList(taskCol, sortMethod) {
 }
 
 function normalizeMilliseconds(milli) {
-    daysLeft = milli / (1000 * 60 * 60 * 24)
-    if (daysLeft >= 1)
+    daysLeft = milli / (1000 * 60 * 60 * 24);
+
+    if (daysLeft >= 1) {
+        
+    }
 }
