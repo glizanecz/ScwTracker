6fd92b5e4973afbd1137567e7d3e2efaaed0c0d4
Greyson Lizanecz <glizanecz@gmail.com>
2026-08-09 00:09:01 -0400

    this is early beta, viewing, editing, deleting, and checking off will be coming soon

--- listdisplay.js
+++ listdisplay.js
@@ -43,18 +43,12 @@ function findDateDistance(dueTime, dueDate) {
 }
 
 function displayList(taskCol, sortMethod) {
-    if (sortMethod === "Automatically (By Due Date)") {
-        let allTasks = sortAutomatically(taskCol);
-        console.log("sorted tasks: ", allTasks);
-        for (let i = 0; i < allTasks.length; i++) {
-            taskList.innerHTML += "<div class='taskListItem'> <h4>• " + allTasks[i].projName + " " + normalizeDate(allTasks[i].projDueDate) + " => " + timeNormalizer(allTasks[i].projDueTime) + "</h4>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> View </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> Edit </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> ❎ </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> ✅ </button> </div>";
-        }
-    } else if (sortMethod === "Manually") {
-        let allTasks = sortByPriority(taskCol);
-        console.log("sorted tasks: ", allTasks);
-        for (let i = 0; i < allTasks.length; i++) {
-            taskList.innerHTML += "<div class='taskListItem'> <h4>• " + allTasks[i].projName + " " + normalizeDate(allTasks[i].projDueDate) + " => " + timeNormalizer(allTasks[i].projDueTime) + "</h4>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> View </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> Edit </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> ❎ </button>" + "<button class='loggedButtons' onclick=" + viewProject(i) + "> ✅ </button> </div>";
-        }
+    let allTasks;
+    sortMethod === "Automatically (By Due Date)" ? allTasks = sortAutomatically(taskCol) : allTasks = sortByPriority(taskCol);
+    window.currentTasks = allTasks;
+    console.log("sorted tasks: ", allTasks);
+    for (let i = 0; i < allTasks.length; i++) {
+        taskList.innerHTML += "<div class='taskListItem'> <h4>• " + allTasks[i].projName + " " + normalizeDate(allTasks[i].projDueDate) + " => " + timeNormalizer(allTasks[i].projDueTime) + "</h4>" + `<button class='loggedButtons' onclick='viewProject(${i})'> View </button> <button class='loggedButtons' onclick='editIndividual(${i})'> Edit </button> <button class='loggedButtons' onclick='viewProject(${i})'> ❎ </button> <button class='loggedButtons' onclick='viewProject(${i})'> ✅ </button> </div>`;
     }
 }
 
