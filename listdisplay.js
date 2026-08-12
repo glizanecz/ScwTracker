406be6f6890bc3f0a5357d21cbc548949c83255d
Greyson Lizanecz <glizanecz@gmail.com>
2026-08-11 23:55:14 -0400

    lots of changes

--- listdisplay.js
+++ listdisplay.js
@@ -23,7 +23,7 @@ function findDateDistance(dueTime, dueDate) {
     let date = new Date(year, month - 1, day).getTime();
     let millisecondTotal = 0;
 
-    if (dueTime.includes("AM")) {
+    /*if (dueTime.includes("AM")) {
         hour = Number(dueTime.substring(0,2));
         if (hour === 12) hour = 0;
         minute = Number(dueTime.substring(3,5));
@@ -33,13 +33,16 @@ function findDateDistance(dueTime, dueDate) {
         if (hour !== 12) hour += 12;
         minute = Number(dueTime.substring(3,5));
         dueTime.length > 5 ? second = Number(dueTime.substring(7, 9)) : second = 0;
-    }
+    }*/
+
+    hour = Number(dueTime.substring(0,2))
+    minute = Number(dueTime.substring(3,5));
+    dueTime.length > 5 ? second = Number(dueTime.substring(6, 8)) : second = 0;
 
     millisecondTotal += hour * (1000 * 60 * 60);
     millisecondTotal += minute * 60_000;
     millisecondTotal += second * 1000;
     millisecondTotal += date;
-
     return millisecondTotal - Date.now();
 }
 
@@ -55,18 +58,38 @@ function displayList(taskCol, sortMethod) {
 }
 
 function normalizeMilliseconds(milli) {
-    console.log("milliseconds recieved: " + milli);
+    const daysInCurrentYear = new Date(new Date().getFullYear(), 1, 29).getDate() === 29 ? 366 : 365;
+    millenniumLeft = milli / (1000 * 60 * 60 * 24 * daysInCurrentYear * 1000);
+    centuriesLeft = milli / (1000 * 60 * 60 * 24 * daysInCurrentYear * 100);
+    decadesLeft = milli / (1000 * 60 * 60 * 24 * daysInCurrentYear * 10);
+    yearsLeft = milli / (1000 * 60 * 60 * 24 * daysInCurrentYear);
+    monthsLeft = milli / (1000 * 60 * 60 * 24 * 30.436875);
+    weeksLeft = milli / (1000 * 60 * 60 * 24 * 7);
     daysLeft = milli / (1000 * 60 * 60 * 24);
-    console.log("days left: " + daysLeft);
-    console.log("days left rounded: " + Math.round(daysLeft));
     hoursLeft = milli / (1000 * 60 * 60);
-    console.log("hours left: " + hoursLeft);
-    console.log("hours left rounded: " + Math.round(hoursLeft));
-    if (daysLeft >= 1) {
+    minutesLeft = milli / (1000 * 60);
+    secondsLeft = milli / 1000;
+    if (millenniumLeft >= 1) {
+        return "(Due in " + Math.round(millenniumLeft) + " millennium...)";
+    } else if (millenniumLeft < 1 && centuriesLeft >= 1) {
+        return "(Due in " + Math.round(centuriesLeft) + " centuries...)";
+    } else if (centuriesLeft < 1 && decadesLeft >= 1) {
+        return "(Due in " + Math.round(decadesLeft) + " decades...)";
+    } else if (decadesLeft < 1 && yearsLeft >= 1) {
+        return "(Due in " + Math.round(yearsLeft) + " years...)";
+    } else if (yearsLeft < 1 && monthsLeft >= 1) {
+        return "(Due in " + Math.round(monthsLeft) + " months...)";
+    } else if (monthsLeft < 1 && weeksLeft >= 1) {
+        return "(Due in " + Math.round(weeksLeft) + " weeks...)";
+    } else if (weeksLeft < 1 && daysLeft >= 1) {
         return "(Due in " + Math.round(daysLeft) + " days...)";
-    } else if (daysLeft < 1 && daysLeft > 0) {
+    } else if (daysLeft < 1 && hoursLeft >= 1) {
         return "(Due in " + Math.round(hoursLeft) + " hours...)";
-    } else if (daysLeft <= 0) {
+    } else if (hoursLeft < 1 && minutesLeft > 1) {
+        return "(Due in " + Math.round(minutesLeft) + " minutes...)";
+    } else if (minutesLeft < 1 && secondsLeft > 0) {
+        return "(Due in " + Math.round(secondsLeft) + " seconds!!!)";
+    } else if (secondsLeft <= 0) {
         return ("Task Overdue!")
     }
 }
